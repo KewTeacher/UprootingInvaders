96924e42463665137db3ade5097e737795f97ac3
Lilith <lvivaudou2022@kewforest.org>
2021-11-29 15:51:17 -0500

    fixed stupid

--- src/run.py
+++ src/run.py
@@ -1,7 +1,16 @@
-from flask import Flask
 
-app = Flask(__name__)
+from mflix.factory import create_app
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+import os
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read(os.path.abspath(os.path.join(".ini")))
+
+if __name__ == "__main__":
+    app = create_app()
+    app.config['DEBUG'] = True
+    app.config['MONGO_URI'] = config['PROD']['DB_URI']
+
+    app.run()

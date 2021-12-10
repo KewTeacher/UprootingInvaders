880c6b9a6522ea3c1d680d036d41d254734fa96f
Jamestvvo <72504203+Jamestvvo@users.noreply.github.com>
2021-12-10 14:22:22 -0500

    commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+import requests
+import json
+
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def index():
+    req = requests.get('https://cat-fact.herokuapp.com/facts')
+    print(req.content)
+    return render_template('index.html')

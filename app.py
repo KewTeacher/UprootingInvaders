3d73616624131f7c80b6457d3b69faa6db2f58a9
Lilith <lvivaudou2022@kewforest.org>
2021-12-15 16:13:08 -0500

    attempting to work from the bottom up
    So I deleted a bunch of code and redid it to see if there were any issues. I found some function ones where they were not properly indented, though got stuck around 4:40 minutes in where it wouldn't display "index" off of the index.html file. I feel like a fix could be rearranging the file order within Windows Explorer since the other files could be interfering with the example API.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask.templating import render_template
 import requests
 import json
 
 app = Flask(__name__)
+
 @app.route('/', methods=['GET'])
 def index():
-    req = requests.get('https://cat-fact.herokuapp.com/facts')
-    print(req.content)
-    return render_template('index.html')
+        req = requests.get('https://cat-fact.herokuapp.com/facts')
+        print(req.content)
+        return render_template('index.html')

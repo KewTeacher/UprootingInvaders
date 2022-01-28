b4484bfbb7c31e1f4c3f9e83f149ebf9de59e5d7
Lilith <lvivaudou2022@kewforest.org>
2022-01-28 14:54:13 -0500

    added whatever james did

--- app.py
+++ app.py
@@ -102,11 +102,38 @@ def logout():
         return render_template('index.html')
 
 
+API_KEY = "2b10FP9NjPISdGPyjNBrQowG"	# Plant API_KEY here
+api_endpoint = f"https://my-api.plantnet.org/v2/identify/all?api-key={API_KEY}"
+
+image_path_1 = "data/image_1.jpeg"
+image_data_1 = open(image_path_1, 'rb')
+
+image_path_2 = "data/image_2.jpeg"
+image_data_2 = open(image_path_2, 'rb')
+
+data = {
+	'organs': ['flower', 'leaf']
+}
+
+files = [
+	('images', (image_path_1, image_data_1)),
+	('images', (image_path_2, image_data_2))
+]
+
+req = requests.Request('POST', url=api_endpoint, files=files, data=data)
+prepared = req.prepare()
+
+s = requests.Session()
+response = s.send(prepared)
+json_result = json.loads(response.text)
+
+print(response.status_code)
+print(json_result)
+
+
 @app.route('/plantid', methods=['GET'])
 def plantid():
-        req = requests.get('https://cat-fact.herokuapp.com/facts')
-        data = json.loads(req.content)
-        return render_template('plantid.html', data=data)
-
-if __name__ == "__main__":
-  app.run(debug=True)
+     #   req = requests.get('https://cat-fact.herokuapp.com/facts')
+     #   req = requests.Request('POST', url=api_endpoint, files=files, data=data)
+         #datas = json.loads(req.results)
+         return render_template('plantid.html', data=json_result)

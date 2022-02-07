3e949e1c4340c938f7df7062eda31e379e921fb4
lanoflatfaceo <lanoflatfaceo@gmail.com>
2022-02-07 08:37:09 -0500

    actual changes -- uploading files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
-from flask import Flask, render_template, request, url_for, redirect, session
+from flask import Flask, flash, render_template, request, url_for, redirect, session
+from werkzeug.utils import secure_filename 
 import pymongo
 import bcrypt
 import requests
@@ -130,6 +131,44 @@ json_result = json.loads(response.text)
 
 print(response.status_code)
 print(json_result)
+
+#Uploading data stuff
+UPLOAD_FOLDER = '/GitHub/UprootingInvaders/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app=FLASK(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 #Plant ID stuff
 @app.route('/plantid', methods=['GET'])

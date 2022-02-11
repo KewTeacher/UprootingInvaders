b2066099884f5fa620b585e095a89c29b2afb515
Jamestvvo <72504203+Jamestvvo@users.noreply.github.com>
2022-02-11 14:43:34 -0500

    commit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pymongo
 import bcrypt
 import requests
 import json
+import os
 
 app = Flask(__name__)
 app.secret_key = "testing"
@@ -133,17 +134,17 @@ print(response.status_code)
 print(json_result)
 
 #Uploading data stuff
-UPLOAD_FOLDER = '/GitHub/UprootingInvaders/uploads'
+UPLOAD_FOLDER = "C:/Users/James/Documents/GitHub/UprootingInvaders/uploads"
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-app=FLASK(__name__)
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -159,7 +160,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
+            return redirect(url_for('upload_file', name=filename))
     return '''
     <!doctype html>
     <title>Upload new File</title>

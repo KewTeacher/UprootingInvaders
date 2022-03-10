d02323dd8b0b8780e914a7372c4ce781b9c364aa
lanoflatfaceo <lanoflatfaceo@gmail.com>
2022-03-10 08:15:26 -0500

    init file setup

--- __init__.py
+++ __init__.py
@@ -1,1 +1,20 @@
-global currentfilename
+from flask import Flask
+
+def creat_app():
+    #Create Flask Application
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+
+    with app.app_conext():
+        #Import parts of our application
+        from .auth import routes
+        from .data import routes
+        from .identifying import routes
+
+        #Register Blueprints
+        app.register_blueprint(auth.auth_bp)
+        app.register_blueprint(data.data_bp)
+        app.register_blueprint(identifying.identifying_bp)
+
+
+        return app

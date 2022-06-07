651fd255a3058928e46e8f2a9c9cf0e935dfccfa
David <daronson@kewforest.org>
2022-06-07 08:34:27 -0400

    Update viewyomama.py
    cleaned up but srtill not working on aws

--- uprooting_invaders/map/viewyomama.py
+++ uprooting_invaders/map/viewyomama.py
@@ -29,17 +29,7 @@ def home():
     invlist = []
     noninvlist = []
 
-    def button():
-        if request.method == 'POST':
-            if request.form.get('action1') == 'VALUE1':
-                pass # do something
-            elif  request.form.get('action2') == 'VALUE2':
-                pass # do something else
-            else:
-                pass # unknown
-        #elif request.method == 'GET':
-            #return render_template('googlemap.html', form=form)
-    #return render_template("googlemap.html")
+    
 
     for i in found:
         dict = {}
